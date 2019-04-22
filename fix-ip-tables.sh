1e0e0ded8fa74608bffd629649a4682d0d5a336e
Bhanuka Yd <bhanuka-yd@users.noreply.github.com>
2019-04-22 17:00:49 +0530

    Update fix-ip-tables.sh

--- fix-ip-tables.sh
+++ fix-ip-tables.sh
@@ -4,16 +4,16 @@
 # change the IP address accoring to your OPENVPN network
 
 
-iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT
-iptables -A INPUT -i eth0 -j DROP
+#iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
+#iptables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT
+#iptables -A INPUT -i eth0 -j DROP
 
 iptables -t nat -F
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 
-echo 'iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT' >> /etc/rc.local
-echo 'iptables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT' >> /etc/rc.local
-echo 'iptables -A INPUT -i eth0 -j DROP' >> /etc/rc.local
+#echo 'iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT' >> /etc/rc.local
+#echo 'iptables -A INPUT -i eth0 -p udp --dport 1194 -j ACCEPT' >> /etc/rc.local
+#echo 'iptables -A INPUT -i eth0 -j DROP' >> /etc/rc.local
 
 echo 'iptables -t nat -F' >> /etc/rc.local
 echo 'iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE' >> /etc/rc.local
